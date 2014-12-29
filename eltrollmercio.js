c42fca9351929447fd632d22eec68569f46d2f98
Nicolas Soldi <nicolas.soldi@gmail.com>
2014-12-29 11:16:26 -0500

    Removed all 'a' tags from articles in uibox class sections

--- eltrollmercio.js
+++ eltrollmercio.js
@@ -14,6 +14,7 @@ if (Meteor.isClient) {
       Session.set("counter", Session.get("counter") + 1);
     }
   });
+
 }
 
 if (Meteor.isServer) {
@@ -21,3 +22,4 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
